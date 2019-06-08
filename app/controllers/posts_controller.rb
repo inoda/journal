885144492678a402c9130482358aebc35e98ba94
Isaac <isaacnoda@gmail.com>
2019-06-08 17:43:15 -0600

    Full crud on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
     @post = Post.new
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+
+    if !@post
+      flash[:error] = "Issue loading post for edit"
+      redirect_to posts_path and return
+    end
+  end
+
+  def show
+    @post = Post.find_by_id(params[:id])
+
+    if !@post
+      flash[:error] = "Issue loading post for edit"
+      redirect_to posts_path and return
+    end
   end
 
   def create
@@ -17,5 +35,29 @@ class PostsController < ApplicationController
       flash.now[:error] = @post.errors.full_messages.join(", ")
       render :new
     end
+  end
+
+  def update
+    @post = Post.find_by_id(params[:id])
+
+    if @post.update(title: params[:title], content: params[:content])
+      flash[:success] = "Entry saved"
+      redirect_to post_path(@post)
+    else
+      flash.now[:error] = @post.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find_by_id(params[:id])
+
+    if post && post.delete
+      flash[:success] = "Entry deleted"
+    else
+      flash[:error] = "Issue deleting entry"
+    end
+
+    redirect_to posts_path
   end
 end
