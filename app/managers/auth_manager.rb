8d27086d960a1a465ddf4597486204c6eeccb454
Isaac <isaacnoda@gmail.com>
2019-06-09 17:59:34 -0600

    Adds check for user

--- app/managers/auth_manager.rb
+++ app/managers/auth_manager.rb
@@ -1,6 +1,7 @@
 class AuthManager
   def self.authenticate(username, password)
     user = User.first
+    return false unless user
     BCrypt::Password.new(user.password) == password && BCrypt::Password.new(user.username) == username
   end
 end
