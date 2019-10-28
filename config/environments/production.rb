baa63d8846d4a9bcd21c4fa9f949816b44bd153e
Isaac <isaacnoda@gmail.com>
2019-10-28 13:34:52 -0600

    Renames app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "private_journal_production"
+  # config.active_job.queue_name_prefix = "journal_production"
 
   config.action_mailer.perform_caching = false
 
