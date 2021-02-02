8e8d51776a9587f3e38c60b9036bfa1016bac0dc
Isaac Noda <isaac.noda@gusto.com>
2021-02-01 23:37:32 -0700

    Adds autosave status display

--- app/assets/javascripts/autosave.js
+++ app/assets/javascripts/autosave.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
   var title = $("body").find("[name=title][data-autosave=true]");
   var content = $("body").find("[name=content][data-autosave=true]");
+  var statusDisplay = $("body").find("[data-autosave=status]");
 
   var autosave = function() {
     $.ajax({
@@ -12,16 +13,24 @@ $(document).ready(function() {
       data: {
         title: title.val(),
         content: content.val(),
-      }
+      },
+      success: function() {
+        statusDisplay.html('Progress is saved &#10003;');
+      },
+      error: function() {
+        statusDisplay.text('Failed to save progress.');
+      },
     });
   };
 
   $(title).on('input', function() {
+    statusDisplay.text('Saving progress...');
     clearTimeout(typingTimer);
     typingTimer = setTimeout(autosave, doneTypingInterval);
   });
 
   $(content).on('input', function() {
+    statusDisplay.text('Saving progress...');
     clearTimeout(typingTimer);
     typingTimer = setTimeout(autosave, doneTypingInterval);
   });
